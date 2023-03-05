99179af81da24fe5bfeebe91be85df2d1f5c333d
Rose Ludovic <NuggaN85@users.noreply.github.com>
2023-03-05 21:28:23 +0100

    Update erreur.php

--- error/erreur.php
+++ error/erreur.php
@@ -2,42 +2,51 @@
 switch($_GET['erreur'])
 {
    case '400':
-   echo utf8_decode( 'Échec de l\'analyse HTTP.' );
-   break;
+     $message = 'Échec de l\'analyse HTTP.';
+     break;
    case '401':
-   echo utf8_decode( 'Le pseudo ou le mot de passe n\'est pas correct !' );
-   break;
+     $message = 'Le pseudo ou le mot de passe n\'est pas correct !';
+     break;
    case '402':
-   echo utf8_decode( 'Le client doit reformuler sa demande avec les bonnes données de paiement.' );
-   break;
+     $message = 'Le client doit reformuler sa demande avec les bonnes données de paiement.';
+     break;
    case '403':
-   echo utf8_decode( 'Requête interdite !' );
-   break;
+     $message = 'Requête interdite !';
+     break;
    case '404':
-   echo utf8_decode( 'La page n\'existe pas ou plus !' );
-   break;
+     $message = 'La page n\'existe pas ou plus !';
+     break;
    case '405':
-   echo utf8_decode( 'Méthode non autorisée.' );
-   break;
+     $message = 'Méthode non autorisée.';
+     break;
    case '500':
-   echo utf8_decode( 'Erreur interne au serveur ou serveur saturé.' );
-   break;
+     $message = 'Erreur interne au serveur ou serveur saturé.';
+     break;
    case '501':
-   echo utf8_decode( 'Le serveur ne supporte pas le service demandé.' );
-   break;
+     $message = 'Le serveur ne supporte pas le service demandé.';
+     break;
    case '502':
-   echo utf8_decode( 'Mauvaise passerelle.' );
-   break;
+     $message = 'Mauvaise passerelle.';
+     break;
    case '503':
-   echo utf8_decode( 'Service indisponible.' );
-   break;
+     $message = 'Service indisponible.';
+     break;
    case '504':
-   echo utf8_decode( 'Trop de temps à la réponse.' );
-   break;
+     $message = 'Trop de temps à la réponse.';
+     break;
    case '505':
-   echo utf8_decode( 'Version HTTP non supportée.' );
-   break;
+     $message = 'Version HTTP non supportée.';
+     break;
    default:
-   echo utf8_decode( 'Erreur !' );
+     $message = 'Erreur !';
 }
-?>
+
+echo '<html>
+        <head>
+          <title>Erreur '.$_GET['erreur'].'</title>
+        </head>
+        <body>
+          <h1>Erreur '.$_GET['erreur'].'</h1>
+          <p>'.utf8_decode($message).'</p>
+        </body>
+      </html>';
